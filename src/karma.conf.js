a5e1a83062e48c8d57624f3533861dd4c8c0e136
CzarEc <czar.echavez@yahoo.co.uk>
2019-02-15 22:04:13 +0000

    NGW-2 & NGW-4: getting e2e tests to work in CI

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -7,7 +7,7 @@ module.exports = function (config) {
     customLaunchers: {
       MyHeadlessChrome: {
         base: 'ChromeHeadless',
-        flags: ['--disable-translate', '--disable-entensions', '--remote-debugging-port=9223', '--no-sandbox']
+        flags: ['--disable-translate', '--disable-extensions', '--remote-debugging-port=8080', '--no-sandbox']
       }
     },
     basePath: '',
